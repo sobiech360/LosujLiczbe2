e09891b4b4e098fb511978683e7b667146e8b102
sobiech360 <sobiech360@gmail.com>
2026-09-01 19:19:17 +0200

    Add files via upload

---
+++ Losuj Liczbe 2/ScoreEntry.cs
@@ -0,0 +1,21 @@
+﻿namespace ZgadnijLiczbe2
+{
+    // ENKAPSULACJA & ABSTRAKCJA: prywatne setter'y chronią przed modyfikacją po utworzeniu
+    public class ScoreEntry
+    {
+        public string Name { get; private set; }
+        public int Attempts { get; private set; }
+        public int Difficulty { get; private set; }
+        public int DurationSeconds { get; private set; }
+        public bool IsNewGamePlus { get; private set; }
+
+        public ScoreEntry(string name, int attempts, int difficulty, int durationSeconds, bool isNewGamePlus = false)
+        {
+            Name = name;
+            Attempts = attempts;
+            Difficulty = difficulty;
+            DurationSeconds = durationSeconds;
+            IsNewGamePlus = isNewGamePlus;
+        }
+    }
+}
